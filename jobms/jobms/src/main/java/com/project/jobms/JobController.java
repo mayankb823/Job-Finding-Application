9d8288fbec5602d5496393db2cb6b8c50fd12a07
Mayank Bhatt <mayankb823@gmail.com>
2024-07-05 18:53:06 +0530

    Implemented circut breaker

--- jobms/jobms/src/main/java/com/project/jobms/JobController.java
+++ jobms/jobms/src/main/java/com/project/jobms/JobController.java
@@ -1,6 +1,7 @@
 package com.project.jobms;
 
 import com.project.jobms.dto.JobDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @RestController
@@ -18,9 +21,17 @@ public class JobController {
     private JobService jobService;
 
     @GetMapping
+    @CircuitBreaker(name="CompanyandRatingBreaker",fallbackMethod = "comapanyRatingFallback")
     public ResponseEntity<List<JobDto>> getAllJobs() {
         List<JobDto> jobs = jobService.findAll();
         return new ResponseEntity<>(jobs, HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<JobDto>> comapanyRatingFallback(Exception ex){
+        List<JobDto> jobs=new ArrayList<>();
+        jobs.add(null);
+
+        return new ResponseEntity<>(jobs,HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
